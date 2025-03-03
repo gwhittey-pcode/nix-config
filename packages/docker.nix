246a7fcd0a1396fc6497b8c2d3b400909b38d3b6
Gerard Whittey <gerard.whittey@gmail.com>
2025-03-03 14:42:57 -0500

    moved docker_contrainer to contrainder

--- packages/docker.nix
+++ packages/docker.nix
@@ -8,21 +8,21 @@
   };
   virtualisation.oci-containers.backend = "docker";
  imports = [
-    ./docker_contriners/jackett.nix
-    ./docker_contriners/medusa.nix
-    ./docker_contriners/plex.nix
-    ./docker_contriners/readarr.nix
-    ./docker_contriners/prowlarr.nix
-    ./docker_contriners/vpn_transmission.nix
-    ./docker_contriners/portainer.nix
-    ./docker_contriners/calibre.nix
-    ./docker_contriners/komga.nix
-    ./docker_contriners/readarr.nix
-    ./docker_contriners/update-containers.nix
-    ./docker_contriners/adguardhome.nix
-    ./docker_contriners/radarr.nix
-    ./docker_contriners/jellyseerr.nix
-    #./docker_contriners/sonarr.nix
+    ./contriners/jackett.nix
+    ./contriners/medusa.nix
+    ./contriners/plex.nix
+    ./contriners/readarr.nix
+    ./contriners/prowlarr.nix
+    ./contriners/vpn_transmission.nix
+    ./contriners/portainer.nix
+    ./contriners/calibre.nix
+    ./contriners/komga.nix
+    ./contriners/readarr.nix
+    ./contriners/update-containers.nix
+    ./contriners/adguardhome.nix
+    ./contriners/radarr.nix
+    ./contriners/jellyseerr.nix
+    #./contriners/sonarr.nix
     
   ];
 }
